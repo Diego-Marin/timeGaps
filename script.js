1118cbb1be285472691af2560b8b003351439842
Diego Mejia <diego.mejia@pascualbravo.edu.co>
2023-08-22 00:29:32 -0500

    Actualizacion de la interface, logica del programa y se incluyo nuevas secciones en el proyecto

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-/* Cronometro */
+/****************** Cronometro **********************/
+
 // Seleccionar los botones.
 const botonInicioPausa = document.querySelector('#boton-inicio-pausa');
 const botonReiniciar = document.querySelector('#boton-reiniciar');
 
 // Variables para almacenar los segundos, minutos y horas.
-let [segundos, minutos, horas] = [0, 0, 0];
+let segundos = 0;
+let minutos = 0;
+let horas = 0;
 
 // Variables para almacenar el intervalo de tiempo que debe
 // transcurrir para actualizar el cronometro y el estado 
@@ -41,82 +44,111 @@ function asignarFormato(unidadDeTiempo) {
   return unidadDeTiempo < 10 ? '0' + unidadDeTiempo : unidadDeTiempo;
 }
 
-botonInicioPausa.addEventListener('click', function() {
+botonInicioPausa.addEventListener('click', function () {
   if (estadoCronometro === 'pausado') {
-    // LLamar a la funcion cronometro cada 1000 milisegundos.
     intervaloDeTiempo = window.setInterval(actualizarCronometro, 1000);
-    // Si el cronometro esta pausado, se muestra la flecha >
-    // y se debe cambiar a || porque va a iniciar.
-    botonInicioPausa.classList.remove('iniciar');
-    botonInicioPausa.classList.add('pausar');
-    botonInicioPausa.innerHTML = 'Pausar';
-    // Actualizar el estado del cronometro.
     estadoCronometro = 'andando';
+    botonInicioPausa.innerHTML = 'Pausar';
   } else {
-    // Detener el cronometro al eliminar el intervalo de tiempo 
-    // usado para llamar a la funcion actualizarCronometro().
     window.clearInterval(intervaloDeTiempo);
-    // Actualizar los botones y el estado del cronometro.
-    botonInicioPausa.classList.remove('pausar');
-    botonInicioPausa.classList.add('iniciar');
-    botonInicioPausa.innerHTML = 'Iniciar';
     estadoCronometro = 'pausado';
+    botonInicioPausa.innerHTML = 'Iniciar';
+  }
+
+  if (estadoCronometro === 'andando') {
+    intervaloProgressBar = setInterval(() => {
+      const progressBar = getProgressBar();
+      if (progressValue < progressBar.max) {
+        progressValue++;
+        progressBar.value = progressValue;
+
+        if ((elementProgressBar === 1 || elementProgressBar === 3 || elementProgressBar === 5 || elementProgressBar === 7) && progressValue === 1500) {
+          elementProgressBar += 1;
+          progressValue = 0;
+        } else if ((elementProgressBar === 2 || elementProgressBar === 4 || elementProgressBar === 6) && progressValue === 300) {
+          elementProgressBar += 1;
+          progressValue = 0;
+        } else if (elementProgressBar === 8 && progressValue === 900) {
+          progressValue = 0;
+        }
+        reproducirAudio(audioElement);
+      }
+    }, 1000);
+  } else {
+    window.clearInterval(intervaloProgressBar);
+    progressValue = 0;
   }
 });
 
-// Reiniciar el cronometro eliminando el intervalo de tiempo,
-// reiniciando los segundos, minutos y horas, y actualizando
-// el estado del cronometro y de los botones.
-botonReiniciar.addEventListener('click', function() {
-  // Eliminar el intervalo.
+botonReiniciar.addEventListener('click', function () {
   window.clearInterval(intervaloDeTiempo);
-
-  // Segundos, minutos y horas.
   segundos = 0;
   minutos = 0;
   horas = 0;
   document.getElementById('cronometro').innerHTML = '00:00:00';
 
   // Botones.
-  document.getElementById('boton-inicio-pausa').innerHTML = 'pausa';
-  botonInicioPausa.classList.remove('pausar');
-  botonInicioPausa.classList.add('iniciar');
-  botonInicioPausa.innerHTML = 'Play';
-
+  botonInicioPausa.innerHTML = 'iniciar';
   // Estado.
   estadoCronometro = 'pausado';
+
+  // Barra de progreso.
+  let elementosProgressBar = document.getElementsByTagName('progress');
+  for (let i = 0; i < elementosProgressBar.length; i++) {
+    elementosProgressBar[i].value = 0;
+  }
+  elementProgressBar = 1;
+  window.clearInterval(intervaloProgressBar);
 });
 
-// Barra de progreso del cronometro
-let timerInterval; // Variable para almacenar el intervalo del cronómetro
+/****************** Barra de progreso del cronometro **********************/
+
+//Variables para aumentar la barra de progreso
 let progressValue = 0; // Valor actual de la barra de progreso
-let step = 1;
+let intervaloProgressBar; // Variable para almacenar el intervalo del cronómetro
 
-botonInicioPausa.addEventListener("click", () => {
-  // Inicia el cronómetro actualizando la barra de progreso cada 1000 ms (1 segundo)
-  timerInterval = setInterval(() => {
-    const progressBar = getProgressBar();
-    if (progressValue < progressBar.max) {
-      progressValue++;
-      progressBar.value = progressValue;
-      if (progressValue === 1500) {
-        step += 1;
-      }
-    } else {
-      clearInterval(timerInterval);
-      progressValue = 0;
-    }
-  }, 1000);
-});
+let elementProgressBar = 1;
 
 const getProgressBar = function () {
   let elementProgress;
 
-  if (step === 1) {
+  if (elementProgressBar === 1) {
     elementProgress = 'pomodoro1';
-  } else if (step === 2) {
+  } else if (elementProgressBar === 2) {
     elementProgress = 'break1';
+  } else if (elementProgressBar === 3) {
+    elementProgress = 'pomodoro2';
+  } else if (elementProgressBar === 4) {
+    elementProgress = 'break2';
+  } else if (elementProgressBar === 5) {
+    elementProgress = 'pomodoro3';
+  } else if (elementProgressBar === 6) {
+    elementProgress = 'break3';
+  } else if (elementProgressBar === 7) {
+    elementProgress = 'pomodoro4';
+  } else if (elementProgressBar === 8) {
+    elementProgress = 'break4';
   }
   const elementosBarProgreso = document.getElementById(elementProgress);
   return elementosBarProgreso;
+};
+
+const audioElement = new Audio('./assets/sound/tono.mp3');
+
+function reproducirAudio(audioElement) {
+  if (elementProgressBar === 2 && progressValue === 0) {
+    audioElement.play();
+  } else if (elementProgressBar === 3 && progressValue === 0) {
+    audioElement.play();
+  } else if (elementProgressBar === 4 && progressValue === 0) {
+    audioElement.play();
+  } else if (elementProgressBar === 5 && progressValue === 0) {
+    audioElement.play();
+  } else if (elementProgressBar === 6 && progressValue === 0) {
+    audioElement.play();
+  } else if (elementProgressBar === 7 && progressValue === 0) {
+    audioElement.play();
+  } else if (elementProgressBar === 8 && progressValue === 0) {
+    audioElement.play();
+  }
 }
